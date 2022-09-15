17cd294c2cdcac188bae6deb5db203eb66f37a60
Wilder Chacón Rojas <will.rojas001@gmail.com>
2022-09-14 21:59:12 -0500

    Creando nuevo Propietario

--- PROGRAMA_BOVINO.consola/Program.cs
+++ PROGRAMA_BOVINO.consola/Program.cs
@@ -9,29 +9,51 @@ namespace PROGRAMA_BOVINO.consola
     class Programa 
     {
         private static I_Repo_Vacas _Repo_Vacas = new Rep_Vacas (new persistencia.AppContext());
+        private static I_Repo_Propietario _Repo_Propietario = new Repo_Propietario (new persistencia.AppContext());
         static void Main (string[] args)
         {
-            Console.WriteLine("Hola Mundo");
-            AdicionarVaca();
-            //buscarvaca(5);
+            Console.WriteLine("Hola Mundo propietario");
+            //AdicionarVaca();
+            AdicionarPropietario();
+            
         }
         private static void AdicionarVaca()
         {
             var Vaca = new Aper_vaca()
             {
                 
-                Cod_Vaca      = "888",
-                Nombre              = "Paloma",
-                Color            = "Cafe",
+                Cod_Vaca      = "777",
+                Nombre              = "Lupe",
+                Color            = "Gris",
                 Raza           = "Brahman",
-                Edad            = "4",
-                Propietario              = "Pedro",
-                Veterinario              = "Carlos",
-                Ubicacion                  = "Finca el trebol"
+                Edad            = "2",
+                Propietario              = "Jonathan Val",
+                Veterinario              = "Santiago",
+                Ubicacion                  = "Finca el paraiso"
 
 
             };
             _Repo_Vacas.AddVaca(Vaca);
+            
+        }
+        private static void AdicionarPropietario()
+        {
+            var Propietario = new Aper_propietario()
+            {
+                
+                Identificacion      = 102236522,
+                Nombre              = "Juan C",
+                Apellido            = "Perez",
+                Direccion           = "kilometro 9 chia-bog",
+                Telefono            = "313526402",
+                E_mail              = "JuanC@correo.com",
+                Genero              = "Masculino",
+                Id_Propietario      = 999,
+                Nombre_Hacienda     = "Finca el paraiso"
+
+
+            };
+            _Repo_Propietario.AddPropietario(Propietario);
             
         }
         //private static void buscarvaca(int idVaca)
--- bovino.dominio/Entidades/Aper_propietario.cs
+++ bovino.dominio/Entidades/Aper_propietario.cs
@@ -5,7 +5,7 @@ namespace bovino.dominio
     public class Aper_propietario: Persona
     {
         public int id{get;set;}
-        //public int Id_Propietario{get;set;}
+        public int Id_Propietario{get;set;}
         public string Nombre_Hacienda{get;set;}
 
     }
